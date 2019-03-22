0485442ec304932df14e85b24251daaa64724c70
Mario Díaz-Ufano <mariodiazu60@gmail.com>
2019-03-22 21:30:15 +0100

    Comienzo del canvas
    Implementado un canvas y una función que nos permite saber la coordenada relativa de nuestro click sobre el canvas.

--- archivo.js
+++ archivo.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
     console.log("Documento listo");
     
+    //CON JQUERY
+    $("#btn-1").click(function()
+    {
+        //Position devuelve la distancia a los bordes, 
+        //con la dist al borde superior e izq obtenemos las coord X e Y
+        var coordenadas = $("#btn-1").position();
+        console.log("X ---> " + coordenadas.top + " Y ----> " + coordenadas.left);
+    }); 
+    
+    /* CON VANILLA
+    function PosicionPuntos(elm)
+    {
+        var elemento = document.getElementById(elm.id);
+        var posicion = elemento.getBoundingClientRect();
+        console.log(posicion.top, posicion.left);
+    }
+    */
+
+        $("#myCanvas").mouseup(function(e)
+        {
+            var coordenadas = $("#myCanvas").position();
+            console.log("Coordenadas Y en Canvas--> " + Math.round(e.clientY - coordenadas.top -48 ));
+            
+        })
+   
+   
 });
+
+
+
+ 	  	
