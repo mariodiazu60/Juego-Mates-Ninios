14b73c13c2783e8f030d797d02286bf6d4d9b604
Mario Díaz-Ufano <mariodiazu60@gmail.com>
2019-03-23 15:17:33 +0100

    Mejora detección coordenadas del click sobre canvas.
    Arreglado el problema de deformación del canvas según la relación de aspecto.
    Mejorada la detección del click sobre el canvas, ahora se tiene en cuenta el margin para el cálculo y funciona en Chrome, Firefox y Edge.

--- archivo.js
+++ archivo.js
@@ -19,10 +19,36 @@ $(document).ready(function() {
     }
     */
 
+        $("#myCanvas").attr("width",$("#myCanvas").width());
+        $("#myCanvas").attr("height",$("#myCanvas").height());
+        $("#myCanvas").hover(function(e)
+        {
+            var pointSize = 3; // Cambia el tamaño del punto
+            var ctx = document.getElementById("myCanvas").getContext("2d");
+            ctx.fillStyle = "#ff2626"; // Color rojo
+
+                ctx.beginPath(); // Iniciar trazo
+                ctx.arc(150,75,50,0,(Math.PI*2),true);
+                ctx.fill(); // Terminar trazo
+        })
+
         $("#myCanvas").mouseup(function(e)
         {
+           
+            //outerxxxxx(true) nos da el ancho del elemento + sus margenes, outerxxxxx([false]) sin margenes
+            //Dividimos el resultado entre dos para eliminar uno de los dos margenes del resultado
+            var marginX = (($("#myCanvas").outerWidth(true)-$("#myCanvas").outerWidth())/2);
+            var marginY = (($("#myCanvas").outerHeight(true)-$("#myCanvas").outerHeight())/2);
             var coordenadas = $("#myCanvas").position();
-            console.log("Coordenadas Y en Canvas--> " + Math.round(e.clientY - coordenadas.top -48 ));
+            var coordClickCanvas = [e.clientX - coordenadas.left - marginX,e.clientY - coordenadas.top - marginY]
+            console.log(coordClickCanvas[0] + " - " + coordClickCanvas[1]);
+
+            var pointSize = 3; // Cambia el tamaño del punto
+            var ctx = document.getElementById("myCanvas").getContext("2d");
+            ctx.fillStyle = "white"; // Color rojo
+            ctx.beginPath(); // Iniciar trazo
+            ctx.arc(coordClickCanvas[0], coordClickCanvas[1], pointSize, 0, Math.PI * 2, true); // Dibujar un punto usando la funcion arc
+            ctx.fill(); // Terminar trazo
             
         })
    
