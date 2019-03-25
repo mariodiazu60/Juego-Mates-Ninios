f3c875dc86a6cfe63a143de3a77055ee328c0a56
Mario Díaz-Ufano <32886429+mariodiazu60@users.noreply.github.com>
2019-03-25 12:50:32 +0100

    Introducuiendo img al canvas

--- archivo.js
+++ archivo.js
@@ -1,44 +1,48 @@
 $(document).ready(function() {
     console.log("Documento listo");
 
+    $("#myCanvas").ready(function()
+    {
+        //Solución al problema de deformación según ratio del canvas
         $("#myCanvas").attr("width",$("#myCanvas").width());
         $("#myCanvas").attr("height",$("#myCanvas").height());
+
+        //Variables para el juego
         var contador = 0;
         var firstClick = [];
         var secondClick = [];
-        var c = document.getElementById("myCanvas");
-        var ctx = c.getContext("2d");
+        var ctx = document.getElementById("myCanvas").getContext("2d");
+        var img = new Image();
+        img.src = "1.jpg";
+        ctx.drawImage(img, 34, 12);
 
+    
         $("#myCanvas").mouseup(function(e)
         {
             //CAPTURAMOS LOS CLICKS SOBRE EL CANVAS
             //outerWH(true) da el ancho del elemento + sus margenes, outerHW() sin margenes
             var marginX = (($("#myCanvas").outerWidth(true)-$("#myCanvas").outerWidth())/2);
             var marginY = (($("#myCanvas").outerHeight(true)-$("#myCanvas").outerHeight())/2);
-            //.position() nos devuelve la distancia hasta el borde de la página --- Con Vanilla sería elementoHTML.getBoundingClientRect().top/left;
+            //.position() nos devuelve la distancia hasta el borde de la página 
+            //Con Vanilla sería elementoHTML.getBoundingClientRect().top/left;
             var coordenadas = $("#myCanvas").position();  
 
-            
             contador ++;
-            console.log("Contador = " + contador);
             if(contador%2 != 0)
             {
                 firstClick = [e.clientX - coordenadas.left - marginX, e.clientY - coordenadas.top - marginY];
-                console.log("Coordenadas prim click = " + firstClick[0] + " - " + firstClick[1]);
                 ctx.beginPath();
                 ctx.fillStyle = "red";
                 ctx.lineWidth = 8;
                 ctx.arc(firstClick[0], firstClick[1], 11, 0, 2 * Math.PI, false);
                 ctx.fill();
                 ctx.stroke();
-                ctx.closePath();
-               
+                ctx.closePath(); 
             }
                 else
                 {
                     secondClick = [e.clientX - coordenadas.left - marginX, e.clientY - coordenadas.top - marginY];
-                    console.log("Coordenadas seg click = " + secondClick[0] + " - " + secondClick[1]);
-                    
+                 
                     ctx.beginPath();
                     ctx.fillStyle = "green";
                     ctx.lineWidth = 8;
@@ -53,10 +57,10 @@ $(document).ready(function() {
                     ctx.lineTo (secondClick[0], secondClick[1]);
                     ctx.stroke(); 
                     ctx.closePath();
+                    console.log("Coordenadas seg click = " + secondClick[0] + " - " + secondClick[1]);
                 }
-
-
         })
+    })
    
 });
 
