0329828ca749e9d550ffa6373c0363d708d3ddd6
Toni Hernández Velasco <toni.herve309@hotmail.com>
2019-03-26 19:52:54 +0100

    Vamos Bien

--- archivo.js
+++ archivo.js
@@ -1,44 +1,5 @@
 $(document).ready(function() {
     console.log("Documento listo");
-    
-    function sumar(num1, num2) {
-    	let res = num1 + num2;
-
-    	return res;
-    }
-
-    function restar(num1, num2) {
-    	let res = num1 - num2;
-
-    	return res;
-    }
-
-    function generarNumeros() {
-    	let ran1 = Math.floor((Math.random()*9)+1),
-    	ran2 = Math.floor((Math.random()*9)+1);
-
-    	return [ran1, ran2];
-    }
-
-    function colocarNumeros() {
-    	let numeros = generarNumeros(),
-        output1 = "<figure>", output2 = output1; 
-
-        for(let i=0;i<numeros[0];i++) {
-            output1 += "<img src='assets/apple.png' class='imagen'>";
-        }
-
-        for(let i=0;i<numeros[1];i++) {
-            output2 += "<img src='assets/apple.png' class='imagen'>";
-        }
-    	
-        output1 += "</figure>";
-        output2 += "</figure>"
-        $("#imagenes1").html(output1);
-        $("#imagenes2").html(output2);
-
-    	return false;
-    }
 
     $("#draggable").draggable({
         // opacity : 0.7, 
@@ -60,8 +21,47 @@ $(document).ready(function() {
                 $(this).wrapInner("<div class = 'outerwrapper'></div>");
             }
             $(".outerwrapper").append(cloned)
-            $(this).css("height", "100%");
+            $(this).css("width", "90%");
         },
         scope: 1
     })
 });
+
+    function sumar(num1, num2) {
+        let res = num1 + num2;
+
+        return res;
+    }
+
+    function restar(num1, num2) {
+        let res = num1 - num2;
+
+        return res;
+    }
+
+    function generarNumeros() {
+        let ran1 = Math.floor((Math.random()*9)+1),
+        ran2 = Math.floor((Math.random()*9)+1);
+
+        return [ran1, ran2];
+    }
+
+    function colocarNumeros() {
+        let numeros = generarNumeros(),
+        output1 = "<figure>", output2 = output1; 
+        console.log("Hola");
+        for(let i=0;i<numeros[0];i++) {
+            output1 += "<img src='assets/apple.png' class='imagen'>";
+        }
+
+        for(let i=0;i<numeros[1];i++) {
+            output2 += "<img src='assets/apple.png' class='imagen'>";
+        }
+        
+        output1 += "</figure>";
+        output2 += "</figure>"
+        document.getElementById("imagenes1").innerHTML = output1;
+        document.getElementById("imagenes2").innerHTML = output2;
+
+        return false;
+    }
